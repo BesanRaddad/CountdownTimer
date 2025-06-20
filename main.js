560921880389d595d0267048666dccede6851ffa
BesanRaddad <144612205+BesanRaddad@users.noreply.github.com>
2025-06-20 19:39:28 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+
+
+let timer;
+
+function startCountdown() {
+  clearInterval(timer);
+  const input = document.getElementById("dateInput").value;
+  const targetDate = flatpickr.parseDate(input, "d/m/Y  h:i K").getTime();
+
+
+  if (!input || isNaN(targetDate)) {
+    document.getElementById("done-msg").innerText = "❌ Please select a valid date.";
+    return;
+  }
+
+  document.getElementById("countdown-container").style.display = "flex";
+  document.getElementById("done-msg").innerText = "";
+
+  function updateCountdown() {
+    const now = new Date().getTime();
+    const difference = targetDate - now;
+
+    if (difference <= 0) {
+      clearInterval(timer);
+      document.getElementById("done-msg").innerText = " Time is up 🎉!";
+      document.getElementById("countdown-container").style.display = "none";
+      return;
+    }
+
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    document.querySelector("#days .number").innerText = days;
+    document.querySelector("#hours .number").innerText = hours;
+    document.querySelector("#minutes .number").innerText = minutes;
+    document.querySelector("#seconds .number").innerText = seconds;
+  }
+
+  timer = setInterval(updateCountdown, 1000);
+  updateCountdown();
+}
+
